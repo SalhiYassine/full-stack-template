ca30d03dab1f1c1bc69717b24d77f2066edc0e47
Yassine Salhi <yassinesalhi135@gmail.com>
2021-11-06 19:33:46 +0000

    base completed

--- server/Server.ts
+++ server/Server.ts
@@ -1,28 +1,25 @@
-
-import {Express, Request, Response} from "express";
-import express from "express";
-import * as path from "path";
+import { Express, Request, Response } from 'express';
+import express from 'express';
+import * as path from 'path';
 
 export class Server {
+  private app: Express;
 
-    private app: Express;
+  constructor(app: Express) {
+    this.app = app;
 
-    constructor(app: Express) {
-        this.app = app;
+    this.app.use(express.static(path.resolve('./') + '/build/frontend'));
 
-        this.app.use(express.static(path.resolve("./") + "/build/frontend"));
+    this.app.get('/api', (req: Request, res: Response): void => {
+      res.send('You have reached the API!');
+    });
 
-        this.app.get("/api", (req: Request, res: Response): void => {
-            res.send("You have reached the API!");
-        });
+    this.app.get('*', (req: Request, res: Response): void => {
+      res.sendFile(path.resolve('./') + '/build/frontend/index.html');
+    });
+  }
 
-        this.app.get("*", (req: Request, res: Response): void => {
-            res.sendFile(path.resolve("./") + "/build/frontend/index.html");
-        });
-    }
-
-    public start(port: number): void {
-        this.app.listen(port, () => console.log(`Server listening on port ${port}!`));
-    }
-
+  public start(port: number): void {
+    this.app.listen(port, () => console.log(`Server listening on port ${port}!`));
+  }
 }
